d5581a9a5ec67c4de8a5c3b3774a367c68d01f2b
YeoHT <jht040403@yahoo.com>
2015-08-10 01:21:17 +0800

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,24 @@
+#Read file
+epc <- read.table("household_power_consumption.txt",sep=";",header=TRUE)
+
+#Date conversion
+epc$Date <- as.Date(epc$Date, format="%d/%m/%Y")
+extract <- epc[(epc$Date=="2007-02-01") | (epc$Date=="2007-02-02"),]
+
+#Convert to numeric
+extract$Global_active_power <- as.numeric(as.character(extract$Global_active_power))
+extract$Sub_metering_1 <- as.numeric(as.character(extract$Sub_metering_1))
+extract$Sub_metering_2 <- as.numeric(as.character(extract$Sub_metering_2))
+extract$Sub_metering_3 <- as.numeric(as.character(extract$Sub_metering_3))
+extract$Voltage <- as.numeric(as.character(extract$Voltage))
+extract <- transform(extract, datetime=as.POSIXct(paste(Date, Time)))
+
+#plot2
+plot(extract$datetime,
+     extract$Global_active_power,
+     type="l",
+     xlab="",
+     ylab="Global Active Power (kilowatts)"
+)
+dev.copy(png, file="plot2.png", width=480, height=480)
+dev.off()
